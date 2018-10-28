3ca834ce80c569e28bb5f0043bdc6e8ac768e6a5
neverhood671 <nastya-art@yandex.ru>
2018-10-28 21:13:10 +0300

    rename variables

--- js-hw-mishunina/book_api_request.js
+++ js-hw-mishunina/book_api_request.js
@@ -1,11 +1,11 @@
-const fetch = require('node-fetch'),
-    apiUrl = 'https://api.nytimes.com/svc/books/v3',
-    bestSellersPath = '/lists/overview.json',
-    apiKey = 'b25752e4a6414ae690b8c1f9c4073126',
-    publishedDate = '2018-10-28',
-    url = `${apiUrl}${bestSellersPath}?api-key=${apiKey}&published_date=${publishedDate}`;
-    
-fetch(url)
+const API_URL = 'https://api.nytimes.com/svc/books/v3',
+    BESTSELLERS_PATH = '/lists/overview.json',
+    API_KEY = 'b25752e4a6414ae690b8c1f9c4073126',
+    DATE = '2018-10-28';
+
+let requestUrl = `${API_URL}${BESTSELLERS_PATH}?api-key=${API_KEY}&published_date=${DATE}`;
+
+fetch(requestUrl)
     .then(res => res.json())
     .then(res => {
         res.results.lists.forEach(list => {
